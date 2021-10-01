a78e7ac8b05b4461d93d683981dcfaf5594c2f80
Yasmeen <Yasmeen.youssef.alsaedi@gmail.com>
2021-10-01 13:11:32 +0300

    fix the protonet

--- models/protonet.py
+++ models/protonet.py
@@ -25,13 +25,14 @@ class ProtoNet(nn.Module):
         num_classes = self.config['training']['num_classes_per_task']
         batch_size = len(x) // num_classes
         c, h, w = x.shape[1:]
-
+        print(x.shape[1:])
+        print(x)
         embeddings = self.embedder(x) # [num_classes * batch_size, dim]
         print(embeddings)
 
         # TODO(protonet): compute prototypes given the embeddings
         # embeddings.sum()/num_classes
-        prototypes = torch.tensor([ (embed.sum(dim=0)/num_classes).tolist() for embed in embeddings]).view((5,-1)).cuda()
+        prototypes = torch.tensor([ (embed.sum(dim=0)/num_classes).tolist() for embed in embeddings]).view((num_classes)).cuda()
 
         # TODO(protonet): copmute the logits based on embeddings and prototypes similarity
         # You can use either L2-distance or cosine similarity
@@ -42,6 +43,6 @@ class ProtoNet(nn.Module):
         softmax = nn.Softmax(dim=1)
         result = torch.tensor(softmax(embeddings))
         cos = nn.CosineSimilarity(dim=1, eps=1e-6)
-        logits = cos(prototypes, result).view((5,-1))
+        logits = cos(prototypes, result).view((num_classes))
 
         return logits
