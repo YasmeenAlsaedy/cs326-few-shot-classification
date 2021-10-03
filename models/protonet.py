8be8b437642100755b5eb46f969f9696b499e8c1
Yasmeen <Yasmeen.youssef.alsaedi@gmail.com>
2021-10-03 16:35:43 +0300

    fix everything

--- models/protonet.py
+++ models/protonet.py
@@ -33,17 +33,17 @@ class ProtoNet(nn.Module):
         # TODO(protonet): compute prototypes given the embeddings
         # embeddings.sum()/num_classes
         #prototypes = embeddings.sum()/num_classes
-        prototypes  = [pclass.sum()/num_classes for embedd in embeddings for pclass in embedd]
+        prototypes  = [(pclass.sum()/num_classes).item() for embedd in embeddings for pclass in embedd]
         print(prototypes)
         # TODO(protonet): copmute the logits based on embeddings and prototypes similarity
         # You can use either L2-distance or cosine similarity
         # torch.
-        euclidean_dist = lambda x: torch.sqrt(torch.pow(x-prototypes, 2))
+        #euclidean_dist = lambda x: torch.sqrt(torch.pow(x-prototypes, 2))
         softmax = nn.Softmax(dim=1)
         result = softmax(embeddings).clone().detach()
         print(result)
         #logits = torch.tensor([euclidean_dist(r).tolist() for r in result], requires_grad = True).to(self.config['device'])
-        logits =  torch.tensor([self.fun_L(v, prototypes[i]).tolist() for f in result for i, v in enumerate(f)]).clone().detach().view([batch_size, num_classes, -1])
+        logits = torch.tensor([self.fun_L(v, prototypes[i]).tolist() for f in result for i, v in enumerate(f)]).clone().detach().view([batch_size, num_classes, -1]).to(self.config['device'])
         print(logits)
 
         return logits
